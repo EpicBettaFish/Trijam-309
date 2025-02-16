0ddcbf7c18745b9161916db81939f144c8785edd
EpicBettaFish <epicbetafish@gmail.com>
2025-02-16 17:36:08 -0500

    fish masking with scanner

--- new-game-project/scripts/gun.gd
+++ new-game-project/scripts/gun.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+#ADJUSTED IN SHOP
+var maxScopedMovement: float = 20.0
+#END
+
 var scopedIn: bool = false
-@onready var sightMask: Sprite2D = $SightMask
+
+@onready var sight: Sprite2D = $SightParent/Sight
+@onready var sightMask: PointLight2D = $SightParent/Sight/SightMask
+@onready var sightParent: Node2D = $SightParent
+
 var scopeStartPos: Vector2
 
 func _ready():
@@ -14,14 +22,15 @@ func _unhandled_input(event) -> void:
 	if event.is_action_released("scopeIn"):
 		scopeOut()
 	if event is InputEventMouseMotion:
+		sight.global_position = get_global_mouse_position()
 		if scopedIn:
-			sightMask.global_position = get_global_mouse_position()
-			sightMask.global_position = sightMask.global_position.clamp()
+			sight.position = sight.position.limit_length(maxScopedMovement)
 
 func scopeIn() -> void:
 	scopedIn = true
 	sightMask.visible = scopedIn
 	scopeStartPos = get_global_mouse_position()
+	sightParent.global_position = scopeStartPos
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func scopeOut() -> void:
