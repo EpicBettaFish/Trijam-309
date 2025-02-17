3059ade6beff30229f68fd5464fceac29b30676c
LeftyAtHeart <62956483+LeftyAtHeart@users.noreply.github.com>
2025-02-16 19:38:41 -0500

    goon added

---
+++ new-game-project/scripts/goon.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var spawner = get_tree().current_scene.get_node("FishSpawner")
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_fire_timer_timeout():
+	if !spawner.evil_fish.is_empty():
+		spawner.evil_fish.front().hit()
+		$AudioStreamPlayer2D.play()
+		$Sprite2D.frame = 1
+		await get_tree().create_timer(0.4).timeout
+		$Sprite2D.frame = 0
