f631b03f0cff08d7e82d5da77b6a0f9e719e2916
Trilomancer <jjwoodcook@gmail.com>
2025-02-16 21:38:39 -0500

    shop8

--- new-game-project/scripts/shop.gd
+++ new-game-project/scripts/shop.gd
@@ -3,30 +3,73 @@ signal updateValues
 
 @onready var main: Node2D = $"../.."
 
+@onready var fr_price: Label = $Perma/FireRate/FRPrice
+@onready var su_price: Label = $Perma/SpotterCooldown/SUPrice
+@onready var sc_price: Label = $Perma/ScanCooldown/SCPrice
+
+@onready var sr_price: Label = $One/ScanRadius/SRPrice
+@onready var gg_price: Label = $One/GoonGunner/GGPrice
+@onready var gs_price: Label = $One/GoonSpotter/GSPrice
+
+
+
+
+
+
+
+
+
+
+
+var fire_rate_cost = 10
+var spotter_cooldown_cost = 10
+var scan_cooldown_cost = 15 #scan uptime
+var scan_radius_cost = 20
+var goon_cost = 20
+
+
+
+func _ready() -> void:
+	fr_price = fire_rate_cost
+	su_price = spotter_cooldown_cost
+	sc_price = scan_cooldown_cost
+	
+	sr_price = scan_radius_cost
+	gg_price = goon_cost
+	gs_price = goon_cost
+	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggleShop"):
 		visible = !visible
-
+		
+		
 
 func _on_fire_rate_button_up() -> void:
-	Singleton.fire_rate_upgrade += 1
-	emit_signal("updateValues")
-	
+	if Singleton.money >= fire_rate_cost:
+		Singleton.fire_rate_upgrade += 1
+		emit_signal("updateValues")
+		fire_rate_cost *= 1.15
+		
 func _on_spotter_cooldown_button_up() -> void:
-	Singleton.spotter_cooldown_upgrade += 1
-	emit_signal("updateValues")
-
+	if Singleton.money >= spotter_cooldown_cost:
+		Singleton.spotter_cooldown_upgrade += 1
+		emit_signal("updateValues")
+		spotter_cooldown_cost *= 1.15
 
 func _on_scan_cooldown_button_up() -> void:
-	Singleton.scan_cooldown_upgrade += 1
-	emit_signal("updateValues")
-
+	if Singleton.money >= scan_cooldown_cost:
+		Singleton.scan_cooldown_upgrade += 1
+		emit_signal("updateValues")
+		scan_cooldown_cost *= 1.15
 
 
 func _on_scan_radius_button_up() -> void:
-	Singleton.scan_radius_upgrade = true
-	emit_signal("updateValues")
-
+	if Singleton.money >= scan_radius_cost:
+		Singleton.scan_radius_upgrade = true
+		emit_signal("updateValues")
+		scan_radius_cost *= 1.15
 	
 func _on_goon_press(type):
-	main.attemptSpawn(type)
+	if Singleton.money >= goon_cost:
+		main.attemptSpawn(type)
+		goon_cost *= 1.15
