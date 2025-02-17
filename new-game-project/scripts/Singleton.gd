03bd89a511dddd32ca1e6bc6af63067dbe2fbc6c
LeftyAtHeart <62956483+LeftyAtHeart@users.noreply.github.com>
2025-02-16 21:16:40 -0500

    rest func

--- new-game-project/scripts/Singleton.gd
+++ new-game-project/scripts/Singleton.gd
@@ -8,3 +8,11 @@ var fire_rate_upgrade: int
 var spotter_cooldown_upgrade: int
 var scan_cooldown_upgrade: int
 var scan_radius_upgrade: bool
+
+func reset():
+	score = 0
+	money = 0
+	fire_rate_upgrade = 0
+	spotter_cooldown_upgrade = 0
+	scan_cooldown_upgrade = 0
+	scan_radius_upgrade = false
